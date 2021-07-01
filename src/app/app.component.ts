48271a73aa11c9563167503505f2988f6c29ba34
Eydie <you@example.com>
2021-06-30 21:24:43 -0500

    hoping to commit most recent changes to GA6

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Satellite } from './satellite.';
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+
+export class AppComponent {
+  title = 'orbit-report';
+
+  sourceList: Satellite[];
+  constructor() {
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+ 
+          let fetchedSatellites = data.satellites;
+          // TODO: loop over satellites
+          for (let i=0; i < fetchedSatellites.length; i++) {
+            let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+            this.sourceList.push(satellite);
+          }
+          // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+          // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+ 
+       }.bind(this));
+    }.bind(this));
+ 
+ }
+
+
+}
